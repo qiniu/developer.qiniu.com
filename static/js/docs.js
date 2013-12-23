5a7be10ba22b7472d4a685d38f1fb17a5a25bdf4
SunLn <dengchenhua@gmail.com>
2013-12-23 18:18:49 +0800

    add feedback btn  and js css

--- static/js/docs.js
+++ static/js/docs.js
@@ -1,3 +1,7 @@
+var DocsAddResource;
+var DocsFeedback;
+
+
 $(function() {
 
     var url = window.location.pathname.toLowerCase();
@@ -112,11 +116,7 @@ $(function() {
             });
         }
     };
-    // 技术支持 模态窗口
-    $('.js-initFeedBack').on('click', function() {
-        QiniuFeedBack.show();
-        return false;
-    });
+
 
 
     //给API页面所有图片的父元素添加一个居中类
@@ -240,6 +240,19 @@ $(function() {
     });
 
     // 资源下载页提交社区SDK/插件
+
+    DocsAddResource = new Zendesk({
+        'width': 740,
+        'minHeight': 866,
+        'frameId': 'AddDocsResource',
+        'hideFrameId': 'AddDocsResourceHide',
+        'url': 'http://192.168.19.56:8000/zendesk/docs',
+    });
+    if (window.location.hash === '#hide_docs') {
+        window.parent.DocsAddResource.hide();
+    } else {
+        DocsAddResource.init();
+    }
     $('.js-add-resource').on('click', function() {
         var title = $.trim($(this).text());
         var type = title === '提交我的插件/工具' ? 'Plugin' : 'SDK';
@@ -247,13 +260,28 @@ $(function() {
         return false;
     });
 
+    //技术支持模态窗口
+
+    DocsFeedback = new Zendesk({
+        'width': 740,
+        'minHeight': 740,
+        'frameId': 'feedbackFrame',
+        'hideFrameId': 'feedbackFrameHide',
+        'url': 'http://192.168.19.56:8000/zendesk/docs-feedback',
+    });
+    if (window.location.hash === '#hide_feedback') {
+        window.parent.DocsFeedback.hide();
+    } else {
+        DocsFeedback.init();
+    }
+    $('.js-zendesk').on('click', function() {
+        DocsFeedback.show();
+        return false;
+    });
+
     // 资源下载页，社区插件/社区SDK分页插件，暂时未用到 
     // $('.bxslider').bxSlider({
     //     controls: false
     // });
-
-
-
-
 
 });
